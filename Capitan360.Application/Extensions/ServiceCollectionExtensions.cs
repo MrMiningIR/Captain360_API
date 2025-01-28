16f073bf46b3e2b7fc9c7b1138dec7552bbb96c8
David <f00000b@gmail.com>
2025-01-28 17:55:26 +0330

    #Implementation of the Project and Layering

---
+++ Capitan360.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Capitan360.Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddApplication(this IServiceCollection service)
+    {
+    }
+}
+
---
+++ Capitan360.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+namespace Capitan360.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddInfrastructure(this IServiceCollection service,
+            IConfigurationManager configurationManager)
+        {
+        }
+    }
+}
